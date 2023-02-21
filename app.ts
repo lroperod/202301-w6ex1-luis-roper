bb8387fc88c8bde01233a3f632f681c088157b10
luis <luisrym@gmail.com>
2023-02-21 19:59:12 +0100

    request post

--- app.ts
+++ app.ts
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+app.post('/', (req, res) => {
+	res.json('Hello World fucking Indio!');
+});
+
 app.listen(port, () => (`Example app listening on port ${port}`));
--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,7 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.post('/', (req, res) => {
+    res.send('Hello World fucking Indio!');
+});
 app.listen(port, () => (`Example app listening on port ${port}`));
